585496ca3ac7a2131941cf7ce7db697e860e5e2c
SyZdog <45461241+SyZdog@users.noreply.github.com>
2021-02-21 23:59:27 +0800

    about ajax

---
+++ SPB/CGB-Ajax-01/src/main/java/com/cy/CgbActivity01Application.java
@@ -0,0 +1,13 @@
+package com.cy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgbActivity01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgbActivity01Application.class, args);
+	}
+
+}
