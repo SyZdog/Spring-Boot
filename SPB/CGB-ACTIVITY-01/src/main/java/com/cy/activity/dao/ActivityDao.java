0b1c2ec4d2d6e353e74a65167e35025552603f95
SyZdog <45461241+SyZdog@users.noreply.github.com>
2021-04-06 23:30:30 +0800

    3.31

--- SPB/CGB-ACTIVITY-01/src/main/java/com/cy/activity/dao/ActivityDao.java
+++ SPB/CGB-ACTIVITY-01/src/main/java/com/cy/activity/dao/ActivityDao.java
@@ -15,13 +15,16 @@ public interface ActivityDao {
 	//修改
 	@Update("update tb_activity set title=#{title},category=#{category},startTime=#{startTime},endTime=#{endTime},remark=#{remark} where id=#{id}")
 	int updateActivity(Activity entity);
-	//基于ID查询
-	@Select("select * from tb_activity where id=#{id}")
-	Activity findById(Long id);
+	
 	//新增
 	@Insert("insert into tb_activity (title,category,startTime,endTime,state,remark,createdUser,createdTime) values"
 			+ "(#{title},#{category},#{startTime},#{endTime},#{state},#{remark},#{createdUser},#{createdTime})")
 	int insertActivity(Activity entity);
+	
+	//基于ID查询
+	@Select("select * from tb_activity where id=#{id}")
+	Activity findById(Long id);
+	
 	//查询
 	@Select("select * from tb_activity")
 	List<Activity> findActivity();
